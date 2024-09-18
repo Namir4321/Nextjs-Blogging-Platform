c2b005c319681945699374c020bef7008f4b9b82
Namir <nk.namirkhan1@gmail.com>
2024-09-18 19:22:58 +0530

    update-three

--- src/components/HomePage/Userlist.jsx
+++ src/components/HomePage/Userlist.jsx
@@ -3,7 +3,7 @@ const Userlist = ({users}) => {
   return (
     <section className="mt-4 gap-4 container grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
       {users.map((user) => (
-        <UserCard user={user} />
+        <UserCard user={user} key={user.name} />
       ))}
     </section>
   );
