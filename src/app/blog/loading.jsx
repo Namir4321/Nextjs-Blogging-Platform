35376d7e77256a54daaaf7a7870365764dd76374
Namir <nk.namirkhan1@gmail.com>
2024-09-19 23:26:21 +0530

    align loading card

--- src/app/blog/loading.jsx
+++ src/app/blog/loading.jsx
@@ -1,6 +1,6 @@
 import { Skeleton } from "@/components/ui/skeleton";
 const loading = () => {
-  return <Skeleton className="h-[300px] md:h-[500px] w-[900px] rounded" />;
+  return <Skeleton className="flex justify-center items-center h-[300px] md:h-[500px] w-[900px] rounded" />;
 };
 
 export default loading;
